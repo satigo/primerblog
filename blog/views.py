4972345fadc451ff4756fd9920c448775bc212f8
satigo <santiago.jose@outlook.com>
2019-09-20 00:27:52 -0600

    Agregadas vistas y plantillas de Publicaciones

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Publicacion
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
 def post_list(request):
     publicaciones = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/post_list.html', {'publicaciones' : publicaciones})
+
+
+def post_detail(request, pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/post_detail.html', {'publicacion': publicacion})
