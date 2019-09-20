e3d3ea961ebb26791eb10d9069a47f57b3a86100
satigo <santiago.jose@outlook.com>
2019-09-19 19:01:22 -0600

    Usando Extensiones de plantillas

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Publicacion
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    publicaciones = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/post_list.html', {'publicaciones' : publicaciones})
