4c750f5dd810ecf7a6e9653613deb2af4335d716
satigo <santiago.jose@outlook.com>
2019-09-27 01:00:29 -0600

    Agregando edicion y nueva publicacion

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('publicacion/<int:pk>/', views.post_detail, name='post_detail'),
+    path('publicacion/nueva', views.publicacion_nueva, name='publicacion_nueva'),
+    path('publicacion/<int:pk>/editar/', views.publicacion_editar, name='publicacion_editar'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Publicacion
 from django.shortcuts import render, get_object_or_404
-
+from .forms import PublicacionForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
@@ -13,3 +14,31 @@ def post_list(request):
 def post_detail(request, pk):
     publicacion = get_object_or_404(Publicacion, pk=pk)
     return render(request, 'blog/post_detail.html', {'publicacion': publicacion})
+
+def publicacion_nueva(request):
+    if request.method == "POST":    
+        form = PublicacionForm(request.POST)
+        if form.is_valid():
+            publicacion = form.save(commit=False)
+            publicacion.autor = request.user
+            publicacion.fecha_publicacion = timezone.now()
+            publicacion.save()
+            return redirect('post_detail', pk=publicacion.pk)
+
+    else:
+        form = PublicacionForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def publicacion_editar(request, pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    if request.method == "POST":
+        form = PublicacionForm(request.POST, instance=publicacion)
+        if form.is_valid():
+            publicacion = form.save(commit=False)
+            publicacion.autor = request.user
+            publicacion.fecha_publicacion = timezone.now()
+            publicacion.save()
+            return redirect('post_detail', pk=publicacion.pk)
+    else:
+        form = PublicacionForm(instance=publicacion)
+    return render(request, 'blog/post_edit.html', {'form': form})
